16d54314764aa6cde3cdce72ec89b4ce148a9f8f
Nikita Timofeev <stariy95@gmail.com>
2021-02-12 19:14:24 +0300

    GitHub Actions experiments

--- .github/actions/get-pom-version/bash.sh
+++ .github/actions/get-pom-version/bash.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/env bash
 
-VERSION="undefined"
-grep ../../pom.xml "<version>"
+function get_xpath_value {
+    xml=$1
+    path=$2
+    if [ -f "$xml" ]; then
+        # xpath returns <foo>value</foo>, so we need to unpack it
+        value=$( xpath "$xml" "$path" 2>/dev/null | perl -pe 's/^.+?\>//; s/\<.+?$//;' )
+        echo -n "$value"
+    else
+        echo "Invalid xml file \"$xml\"!"
+        exit 1;
+    fi
+}
+
+pom_xml='../../pom.xml'
+
+VERSION=$(    get_xpath_value $pom_xml 'project/version'    )
 
 echo "::set-output name=pom-version::$VERSION"
-echo "TEST_VAR=4.4-SNAPSHOT" >> $GITHUB_ENV
+echo "TEST_VAR=4.4-SNAPSHOT" >> "$GITHUB_ENV"
