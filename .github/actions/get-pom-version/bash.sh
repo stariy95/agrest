39980d95041d4e05fa1cb578d8991b8994f5558e
Nikita Timofeev <stariy95@gmail.com>
2021-02-12 19:35:17 +0300

    GitHub Actions experiments

--- .github/actions/get-pom-version/bash.sh
+++ .github/actions/get-pom-version/bash.sh
@@ -13,7 +13,7 @@ function get_xpath_value {
 
 VERSION=$(get_xpath_value './pom.xml' 'project/version')
 
-echo "$VERSION"
+echo "pom.xml version: $VERSION"
 
 # export VERSION to the GitHub env
 echo "POM_VERSION=$VERSION" >> "$GITHUB_ENV"
